93aef1ca9c12e44e2031735ed6b5f4f4e4c0d8fd
ashtastic <amurr20@gmail.com>
2016-03-08 17:42:18 -0500

    Fixed problems to match instructions.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,20 @@
 var $h1Below = $('<h1>');
-$h1Below.html('Below ground veggies');
-$('body').prepend($h1Below);
-
 var $ulBelow = $('<ul>');
-$h1Below.append($ulBelow);
-
-var $liBelow = $('<li>');
-$ulBelow.prepend($liBelow);
-
-$liBelow.hasClass('below');
-
 var $h1Above = $('<h1>');
-$h1Above.html('Above ground veggies');
-$('body').prepend($h1Above);
-
 var $ulAbove = $('<ul>');
-$h1Above.append($ulAbove);
 
-var $liAbove = $('<li>');
-$ulAbove.prepend($liAbove);
 
-$liAbove.hasClass('above');
+$h1Below.html('Below ground veggies');
+
+$('body').append($h1Below);
+
+$('body').append($ulBelow);
+
+$h1Above.html('Above ground veggies');
+
+$('body').append($h1Above);
+
+$('body').append($ulAbove);
 
 $('ul li').each(function () {
   if ($(this).hasClass('below')){
